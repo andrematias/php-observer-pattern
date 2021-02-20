9c0b8e0c356935ae102e7020489fe05dcdad495b
André Matias <dev.andrematias@gmail.com>
2021-02-19 22:27:01 -0300

    Inject the Bus into MessageMQEventSubscriber

--- index.php
+++ index.php
@@ -5,10 +5,19 @@ use Mutant\EventPublisher;
 use Mutant\Editor;
 use Mutant\EmailLoggerSubscriber;
 use Mutant\FileLoggerSubscriber;
+use Mutant\RabbitMqEventSubscriber;
+use Mutant\SimpleEventBus;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+$simpleBus = new SimpleEventBus(new AMQPStreamConnection('172.19.0.2', 5672, 'root', 'root'));
+$simpleBus->setQueue('task_queue');
 
 $publisher = EventPublisher::getInstance();
+
+$publisher->addSubscriber(new RabbitMqEventSubscriber("FileOpenedEvent", $simpleBus));
+$publisher->addSubscriber(new RabbitMqEventSubscriber("FileSavedEvent", $simpleBus));
+
 $publisher->addSubscriber(new FileLoggerSubscriber("FileOpenedEvent"));
-$publisher->addSubscriber(new FileLoggerSubscriber("FileSavedEvent"));
 $publisher->addSubscriber(new EmailLoggerSubscriber("FileSavedEvent"));
 
 $editor = new Editor("/etc/apache.conf", $publisher);
