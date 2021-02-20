1efbc0bcea72d4afab8e6a609908b1b857e8e917
André Matias <dev.andrematias@gmail.com>
2021-02-19 22:27:27 -0300

    Format Editor messages

--- Editor.php
+++ Editor.php
@@ -9,20 +9,27 @@
         private string $filename;
         private AbstractPublisher $eventPublisher;
 
-        public function __construct(string $filename, AbstractPublisher $publisher) {
+        public function __construct(
+            string $filename,
+            AbstractPublisher $publisher
+        ) {
             $this->filename = $filename;
             $this->eventPublisher = $publisher;
         }
 
         public function openFile() {
-            echo "Editor Abrindo o arquivo {$this->filename} \n";
-            echo "#---------------------------------------------\n";
-            $this->eventPublisher->notifySubscriber(new FileOpenedEvent($this->filename));
+            echo "\nEditor Abrindo o arquivo {$this->filename} \n";
+            echo "#---------------------------------------------\n\n";
+            $this->eventPublisher->notifySubscriber(
+                new FileOpenedEvent($this->filename)
+            );
         }
 
         public function saveFile() {
-            echo "Editor Salvando o arquivo {$this->filename} \n";
+            echo "\nEditor Salvando o arquivo {$this->filename} \n";
             echo "#---------------------------------------------\n";
-            $this->eventPublisher->notifySubscriber(new FileSavedEvent($this->filename));
+            $this->eventPublisher->notifySubscriber(
+                new FileSavedEvent($this->filename)
+            );
         }
     }
